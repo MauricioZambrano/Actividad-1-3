f270114a9cabe5fbc438fdbfdae36f48fcab2017
Mauricio Zambrano <wichoz2000@gmail.com>
2020-09-08 11:12:04 -0500

    Test 2

--- main.cpp
+++ main.cpp
@@ -3,9 +3,9 @@
 #include <string>
 #include <vector>
 
-using namespace std;
-
 #include "Registro.h"
+
+using namespace std;
 
 void cargaRegistros(vector<Registro*> &vecRegistros){
     string mes;
@@ -26,7 +26,83 @@ void cargaRegistros(vector<Registro*> &vecRegistros){
     
 }
 
+int clave(string mes, int dia){
+    if(mes == "Jun")
+            return 600 + dia;
+    if(mes == "Jul")
+        return 700 + dia;
+    if(mes == "Aug")
+        return 800 + dia;
+    if(mes == "Sep")
+        return 900 + dia;
+        
+    return 1000 + dia;
+}
+
+int busqeudaBinariaIni(vector<Registro> v, int dato, bool inicio ){
+    int ini = 0, fin = v.size() - 1,  mit;
+
+    bool flag;
+
+    cantBB = 0;
+
+    while (ini <= fin){
+        mit = (ini + fin) / 2;
+        cantBB++;
+        if (v[mit] == dato){
+            if(inicio && flag == true){
+                while(v[mit].getClave() == dato){
+                    mit--;
+                }
+                flag = false;
+            }
+            else if(!inicio flag == true){
+                while(v[mit].getClave() == dato){
+                    mit++;
+                }
+                flag = false;
+            }
+            return mit;
+        }
+        else if(v[mit] > dato){
+            fin = mit - 1;
+        }
+        else{
+            ini = mit + 1;
+        }
+    }
+    return -1;
+
+}
+
+void imprime(Registro dato){
+    cout << dato.getMes() << " " << dato.getDia() << " " << dato.getHora() << " " << dato.getIp() << " " dato.getRazon() << endl;
+}
+
+void busqueda(vector<Registro> vec, string mesI, string mesF, int diaI, int diaF){
+    int ini = clave(mesI, diaI), posInicial;
+    int fin = clave(mesF, diaF), posFinal;
+
+    posInicial = busqeudaBinariaIni(vec, ini, true);
+    posFinal = busqeudaBinariaIni(vec, fin, false);
+
+    for(int i = posInicial; i<posFinal; i++){
+        imprime(vec[i]);
+    }
+
+}
+
+
 int main(){
     vector<Registro*> vecRegistros;
     cargaRegistros(vecRegistros);
+
+
+    cout << "Fecha inicial: (ej. ago 10)" << endl;
+    cin >> mesI >> diaI;
+
+    cout << endl << "Fecha Final: " << endl;
+    cin >> mesF >> diaF;
+
+    busqueda(listaFechas, mesI, mesF, diaI, diaF);
 }
